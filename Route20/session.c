ea144b2e9978c7d216bc7c171b22fe47ca555567
Supratim Sanyal <tuklusan@users.noreply.github.com>
2026-09-18 08:06:29 -0400

    Fix SessionInitialise table bounds

--- Route20/session.c
+++ Route20/session.c
@@ -74,12 +74,12 @@ static void DataCallback(uint16 locAddr, byte *data, uint16 dataLength);
 void SessionInitialise(void)
 {
     unsigned int i;
-    for (i = 0; i < MAX_SESSIONS; i++)
+    for (i = 0; i < MAX_OBJECTS; i++)
     {
         memset(&ObjectRegistrationTable[i], 0, sizeof(object_registration_t));
     }
 
-    for (i = 0; i < MAX_OBJECTS; i++)
+    for (i = 0; i < MAX_SESSIONS; i++)
     {
         memset(&SessionTable[i], 0, sizeof(session_t));
     }
